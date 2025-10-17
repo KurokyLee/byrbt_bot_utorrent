1a5cf87ca4793eccabc9e0448705065756166ea9
KurokyLee <wugecheng3428@163.com>
2025-10-17 17:40:56 +0800

    remove tem files

--- test.py
+++
@@ -1,9 +0,0 @@
-from DrissionPage import ChromiumPage, ChromiumOptions
-
-co = ChromiumOptions()
-# 启用无头模式
-# co.set_argument('--headless')   # 开启无头模式
-# 或禁用无头模式（让浏览器窗口可见）
-# co.remove_argument('--headless')
-page = ChromiumPage(co)
-page.get('https://byr.pt/')
